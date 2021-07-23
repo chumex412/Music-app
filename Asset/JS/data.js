3fecc3cac8a773d9473ff1bd14a7c03187eccf68
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-07-23 18:44:24 +0100

    Added id to each array elements

---
+++ Asset/JS/data.js
@@ -0,0 +1,53 @@
+const data = () => {
+  return [
+    {
+      id: 0,
+      title: 'We Dont Talk Anymore',
+      artist: "Charlie Puth ft Selena Gomez",
+      file: "song1",
+      cover: "cover1",
+      active: true
+    },
+    {
+      id: 1,
+      title: 'Pretty Little Liars',
+      artist: "Black ft J. Cole",
+      file: "song2",
+      cover: "cover2",
+      active: false
+    },
+    {
+      id: 2,
+      title: 'Up All Night',
+      artist: "Khalid",
+      file: "song3",
+      cover: "cover3",
+      active: false
+    },
+    {
+      id: 3,
+      title: 'Got Money',
+      artist: "Lil Wayne ft T-pain",
+      file: "song4",
+      cover: "cover4",
+      active: false
+    },
+    {
+      id: 4,
+      title: 'Falling Like the Stars',
+      artist: "James Arthur",
+      file: "song5",
+      cover: "cover5",
+      active: false
+    },
+    {
+      id: 5,
+      title: 'Vibration',
+      artist: "Fireboy",
+      file: "song6",
+      cover: "cover6",
+      active: false
+    },
+
+  ]
+}
